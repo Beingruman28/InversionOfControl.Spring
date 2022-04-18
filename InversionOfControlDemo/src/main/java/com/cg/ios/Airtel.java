f2fd1c99c331d610d9d29998666f7b1d805bdfa4
ruman <mohammedruman1996@gmail.com>
2022-04-18 14:11:53 +0530

    first commit in spring.

---
+++ InversionOfControlDemo/src/main/java/com/cg/ios/Airtel.java
@@ -0,0 +1,15 @@
+package com.cg.ios;
+
+public class Airtel implements Sim{
+
+	public void calling() {
+		System.out.println("calling via Airtel");
+		
+	}
+
+	public void browsing() {
+		System.out.println("browsing via Airtel");
+		
+	}
+
+}
---
+++ InversionOfControlDemo/src/main/java/com/cg/ios/Jio.java
@@ -0,0 +1,19 @@
+package com.cg.ios;
+
+public class Jio implements Sim{
+	
+	public Jio() {
+		System.out.println("object is getting created in the background");
+	}
+
+	public void calling() {
+		System.out.println("calling via Jio");
+		
+	}
+
+	public void browsing() {
+		System.out.println("browsing via Jio");
+		
+	}
+
+}
